2f3aee79aa5db2526ba2b88dfe68fdc47f843b8b
TheNeonLeon <leon.listo@hotmail.com>
2023-02-03 03:39:24 +0100

    CHORE: update styling

--- src/components/ventilationOverview/VentilationOverview.tsx
+++ src/components/ventilationOverview/VentilationOverview.tsx
@@ -1,6 +1,7 @@
 import db from "@/firebase/firebaseConfig";
 import { getAreaData, getVentilationData } from "@/utils/ventilationApi";
-import { doc, updateDoc } from "firebase/firestore";
+import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
 import { SetStateAction, useEffect, useState } from "react";
 import CreatePumpModal from "./modal/CreatePumpModal";
 import { AreaDataProps } from "./modal/types";
@@ -12,15 +13,14 @@ export default function VentilationOverview() {
   const [input, setInput] = useState<string>("");
   const [area, setAreaInfo] = useState([]);
 
-  const usePrevious = (event: {
-    target: { value: SetStateAction<string> };
-  }) => {};
+  const router = useRouter();
 
   const updateStatus = async (
     id: string,
     statusCondition: boolean,
     db: any
   ) => {
+
     const userDoc = doc(db, "ventilations", id);
     try {
       const updateFieldData = {
@@ -28,21 +28,27 @@ export default function VentilationOverview() {
           statusCondition: !statusCondition,
         },
       };
-      setStatusCondition(true);
+      
       await updateDoc(userDoc, updateFieldData);
+      setStatusCondition(true);
     } catch (error) {
       console.error("Error updating document: ", error);
     }
+    console.log(await router.replace(router.asPath));
+    router.reload()
   };
 
   const filterArea = area.filter(function(result:any) {
     const filter = area.map((data:AreaDataProps) => data.areaType);
     const areaTypes = result.areaType;
     // console.log(filter.join(''));
-    console.log(areaTypes);
     
     return result.areaType === filter;
   })
+
+  useEffect(() => {
+    getVentilationData(db);
+  }, [statusCondition])
 
   useEffect(() => {
     const getData = async () => {
@@ -118,21 +124,21 @@ console.log(filterArea);
                       <div className="md:flex md:flex-row p-7 border-spacing-5 rounded-3xl bg-white m-12 mb-5 pb-3 justify-evenly sm:flex flex-col">
                         <div className="flex flex-col">
                           <li className="opacity-70">{data.ventilation.pump.pumpDescription}</li>
-                          <li>{data.ventilation.pump.pumpNumber}</li>
+                          <li className="text-xl">{data.ventilation.pump.pumpNumber}</li>
                         </div>
                         <div className="flex flex-col">
                           <p className="opacity-70">Area</p>
-                          <p>{data.ventilation.area.areaType}</p>
+                          <p className="text-xl">{data.ventilation.area.areaType}</p>
                         </div>
-                        <div className="flex flex-col">
+                        <div className="flex flex-col text-xl">
                           <p className="opacity-70">Status</p>
                           {data.status.statusCondition == false ? (
-                            <p>Disabled</p>
+                            <p className="text-red-600">Disabled</p>
                           ) : (
-                            <p>Active</p>
+                            <p className="text-green-600">Active</p>
                           )}
                         </div>
-                        <div className="flex justify-center text-white bg-violet-700 hover:bg-blue-800 md:w-52 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <div className="flex justify-center text-xl text-white bg-violet-700 hover:bg-blue-800 md:w-52 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         {data.status.statusCondition == false ? (
                           <button
                             onClick={() => {
